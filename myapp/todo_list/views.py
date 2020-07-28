fdcf72e0d00946bc589063116f6f1dba7a4445ed
yo14 <ifliandry11@gmail.com>
2020-07-29 05:52:24 +0700

    Passing data form views to templates

--- myapp/todo_list/views.py
+++ myapp/todo_list/views.py
@@ -4,4 +4,8 @@ def home(request):
     return render(request, 'todo_list/home.html', {})
 
 def about(request):
-    return render(request, 'todo_list/about.html', {})
+    context = {
+        'first_name': 'Yolla',
+        'last_name': 'Ifliandry',
+    }
+    return render(request, 'todo_list/about.html', context)
