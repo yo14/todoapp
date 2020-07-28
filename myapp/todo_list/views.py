618f7510fdbb280e2f54287b2cdec9f5fcaaed2d
yo14 <ifliandry11@gmail.com>
2020-07-28 13:36:20 +0700

    Adding Bootstrap 4 to App

--- myapp/todo_list/views.py
+++ myapp/todo_list/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'todo_list/home.html', {})
+
+def about(request):
+    return render(request, 'todo_list/about.html', {})
