ff050d986d479b1d2eb5d069785e288c86c9c1a5
yo14 <ifliandry11@gmail.com>
2020-07-28 13:00:47 +0700

    Using html templates in django

--- myapp/todo_list/views.py
+++ myapp/todo_list/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('hello world')
+    return render(request, 'todo_list/home.html', {})
