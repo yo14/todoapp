6d01b8f2c20ba8435ca76c492c90daf176252fba
yo14 <ifliandry11@gmail.com>
2020-07-29 11:44:59 +0700

    Add a show message after insert a new data

--- myapp/todo_list/views.py
+++ myapp/todo_list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
     if request.method == "POST":
@@ -9,6 +10,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request, ('Item has been added to list!'))
             return render(request, 'todo_list/home.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
