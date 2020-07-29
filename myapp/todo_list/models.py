0981456559c213b43d70600bfc9554d52fb907bb
yo14 <ifliandry11@gmail.com>
2020-07-29 09:56:35 +0700

    Adding models.py into admin.py

--- myapp/todo_list/models.py
+++ myapp/todo_list/models.py
@@ -5,6 +5,6 @@ class List(models.Model):
     completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.item
+        return self.item + " | "+str(self.completed)
 
 
